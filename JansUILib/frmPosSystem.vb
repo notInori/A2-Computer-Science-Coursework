774c86d2389bc1d67c1c9b830cecba8549784f32
ishrainy <nicksu2005@gmail.com>
2023-03-29 19:32:28 +0100

    Full implemented menu tab interface

--- JansUILib/frmPosSystem.vb
+++ JansUILib/frmPosSystem.vb
@@ -71,16 +71,33 @@ Public Class POSSystem
 
         'Adds the first item in the selector
         tblMenuTabsContainer.ColumnCount = 1
-        tblMenuTabsContainer.Controls.Add(New Label With {.Margin = New Padding(0), .Padding = New Padding(5), .Font = UIfont, .AutoSize = True, .Dock = DockStyle.Left, .ForeColor = Color.White, .Text = CStr(MenuCatergories(0))}, 0, 0)
-        tblMenuTabsContainer.Controls.Add(New Panel With {.Size = New Size(0, 1), .Margin = New Padding(0), .Dock = DockStyle.Fill, .BackColor = Color.White}, 0, 1)
+        'tblMenuTabsContainer.Controls.Add(New Label With {.Margin = New Padding(0), .Padding = New Padding(5), .Font = UIfont, .AutoSize = True, .Dock = DockStyle.Left, .ForeColor = Color.White, .Text = CStr(MenuCatergories(0))}, 0, 0)
+        'tblMenuTabsContainer.Controls.Add(New Panel With {.Name = MenuCatergories(0), .Size = New Size(0, 1), .Margin = New Padding(0), .Dock = DockStyle.Fill, .BackColor = Color.White}, 0, 1)
         tblMenuTabsContainer.ColumnStyles.RemoveAt(1)
-        For i As Integer = 1 To MenuCatergories.Count - 1
+        For i As Integer = 0 To MenuCatergories.Count - 1
             tblMenuTabsContainer.ColumnCount += 1
             tblMenuTabsContainer.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.AutoSize))
             'tblMenuTabsContainer.RowStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.AutoSize))
-            tblMenuTabsContainer.Controls.Add(New Label With {.Size = New Size(100, 100), .Margin = New Padding(0), .Padding = New Padding(5), .Font = UIfont, .AutoSize = True, .Dock = DockStyle.Left, .ForeColor = Color.FromArgb(150, 150, 150), .Text = CStr(MenuCatergories(i))}, CInt(tblMenuTabsContainer.ColumnCount), 0)
+            Dim TabLabel As New Label With {.Name = MenuCatergories(i), .Size = New Size(100, 100), .Margin = New Padding(0), .Padding = New Padding(5), .Font = UIfont, .AutoSize = True, .Dock = DockStyle.Left, .ForeColor = Color.FromArgb(150, 150, 150), .Text = CStr(MenuCatergories(i))}
+            tblMenuTabsContainer.Controls.Add(TabLabel, CInt(tblMenuTabsContainer.ColumnCount), 0)
             tblMenuTabsContainer.Controls.Add(New Panel With {.Size = New Size(0, 1), .Margin = New Padding(0), .Dock = DockStyle.Fill, .BackColor = Color.Transparent}, CInt(tblMenuTabsContainer.ColumnCount), 1)
-
+            AddHandler TabLabel.Click, Sub(sender As Object, e As EventArgs)
+                                           Dim currentColumn As Integer = tblMenuTabsContainer.GetColumn(sender)
+                                           For Each cntrl As Control In tblMenuTabsContainer.Controls.OfType(Of Panel)
+                                               If tblMenuTabsContainer.GetColumn(cntrl) = currentColumn Then
+                                                   cntrl.BackColor = Color.White
+                                               Else
+                                                   cntrl.BackColor = Color.Transparent
+                                               End If
+                                           Next
+                                           For Each cntrl As Control In tblMenuTabsContainer.Controls.OfType(Of Label)
+                                               If cntrl Is sender Then
+                                                   cntrl.ForeColor = Color.White
+                                               Else
+                                                   cntrl.ForeColor = Color.FromArgb(150, 150, 150)
+                                               End If
+                                           Next
+                                       End Sub
         Next
         tblMenuTabsContainer.ColumnCount += 2
         tblMenuTabsContainer.Controls.Add(New Panel With {.Size = New Size(0, 1), .Margin = New Padding(0), .Dock = DockStyle.Fill, .BackColor = Color.Transparent}, CInt(tblMenuTabsContainer.ColumnCount), 1)
